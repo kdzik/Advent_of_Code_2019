cb27bb9124f826e757bc5ac33cee03ba74b1b08e
kdzik <krystiandzik06@gmail.com>
2020-02-11 22:59:52 +0100

    Inital Day2 commit

--- src/aoc/kdzik/Main.java
+++ src/aoc/kdzik/Main.java
@@ -1,13 +1,16 @@
 package aoc.kdzik;
 
 import aoc.kdzik.day1.Day1;
+import aoc.kdzik.day2.Day2;
 
 import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Day1 day1 = new Day1();
-        day1.initDay1();
+       // Day1 day1 = new Day1();
+    //    day1.initDay1();
+        Day2 day2 = new Day2();
+        day2.initDay2();
     }
 }
